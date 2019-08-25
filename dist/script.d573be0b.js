841f8277e4ff6d40c4993d78f29a7479ffcbe10a
Zubair Akram <zubair616@gmail.com>
2019-08-25 20:14:52 +0500

    calculator is completed

--- dist/script.d573be0b.js
+++ dist/script.d573be0b.js
@@ -118,7 +118,157 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"js/script.js":[function(require,module,exports) {
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var Calculator =
+/*#__PURE__*/
+function () {
+  function Calculator(previousOperandTextElement, currentOperandTextElement) {
+    _classCallCheck(this, Calculator);
+
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
+  }
+
+  _createClass(Calculator, [{
+    key: "clear",
+    value: function clear() {
+      this.currentOperand = '';
+      this.previousOperand = '';
+      this.operation = undefined;
+    }
+  }, {
+    key: "delete",
+    value: function _delete() {
+      this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    }
+  }, {
+    key: "appendNumber",
+    value: function appendNumber(number) {
+      if (number === '.' && this.currentOperand.includes('.')) return;
+      this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+  }, {
+    key: "chooseOperation",
+    value: function chooseOperation(operation) {
+      if (this.currentOperand === '') return;
+
+      if (this.previousOperand !== '') {
+        this.compute();
+      }
+
+      this.operation = operation;
+      this.previousOperand = this.currentOperand;
+      this.currentOperand = '';
+    }
+  }, {
+    key: "compute",
+    value: function compute() {
+      var computation;
+      var prev = parseFloat(this.previousOperand);
+      var current = parseFloat(this.currentOperand);
+      if (isNaN(prev) || isNaN(current)) return;
+
+      switch (this.operation) {
+        case '+':
+          computation = prev + current;
+          break;
+
+        case '-':
+          computation = prev - current;
+          break;
+
+        case '*':
+          computation = prev * current;
+          break;
+
+        case '÷':
+          computation = prev / current;
+          break;
+
+        default:
+          return;
+      }
+
+      this.currentOperand = computation;
+      this.operation = undefined;
+      this.previousOperand = '';
+    }
+  }, {
+    key: "getDisplayNumber",
+    value: function getDisplayNumber(number) {
+      var stringNumber = number.toString();
+      var integerDigits = parseFloat(stringNumber.split('.')[0]);
+      var decimalDigits = stringNumber.split('.')[1];
+      var integerDisplay;
+
+      if (isNaN(integerDigits)) {
+        integerDisplay = '';
+      } else {
+        integerDisplay = integerDigits.toLocaleString('en', {
+          maximumFractionDigits: 0
+        });
+      }
+
+      if (decimalDigits != null) {
+        return "".concat(integerDisplay, ".").concat(decimalDigits);
+      } else {
+        return integerDisplay;
+      }
+    }
+  }, {
+    key: "updateDisplay",
+    value: function updateDisplay() {
+      this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand);
+
+      if (this.operation != null) {
+        this.previousOperandTextElement.innerText = "".concat(this.getDisplayNumber(this.previousOperand), " ").concat(this.operation);
+      } else {
+        this.previousOperandTextElement.innerText = '';
+      }
+    }
+  }]);
+
+  return Calculator;
+}();
+
+var numberButtons = document.querySelectorAll('[data-number]');
+var operationButtons = document.querySelectorAll('[data-operation]');
+var equalsButton = document.querySelector('[data-equals]');
+var deleteButton = document.querySelector('[data-delete]');
+var allClearButton = document.querySelector('[data-all-clear]');
+var previousOperandTextElement = document.querySelector('[data-previous-operand]');
+var currentOperandTextElement = document.querySelector('[data-current-operand]');
+var calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+numberButtons.forEach(function (button) {
+  button.addEventListener('click', function () {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+operationButtons.forEach(function (button) {
+  button.addEventListener('click', function () {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+equalsButton.addEventListener('click', function (button) {
+  calculator.compute();
+  calculator.updateDisplay();
+});
+allClearButton.addEventListener('click', function (button) {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+deleteButton.addEventListener('click', function (button) {
+  calculator.delete();
+  calculator.updateDisplay();
+});
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +297,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46745" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40131" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
